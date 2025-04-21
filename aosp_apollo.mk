ac4daff3e2258531f8013764720bd8d8a6978f34
redznn <redznn@protonmail.com>
2025-04-21 15:44:13 +0000

    apollo: Import build fingerprint * Forget during decommon
    Signed-off-by: redznn <redznn@protonmail.com>

--- aosp_apollo.mk
+++ aosp_apollo.mk
@@ -33,3 +33,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Xiaomi Mi 10T
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="apollo_global-user 12 RKQ1.211001.001 V14.0.4.0.SJDMIXM release-keys" \
+    BuildFingerprint=Xiaomi/apollo_global/apollo:12/RKQ1.211001.001/V14.0.4.0.SJDMIXM:user/release-keys
